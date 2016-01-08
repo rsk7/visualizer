49b9b4ff6dde47aa0b4c61b8c19d8df4b8b4391d
rsk7 <ravikrishnasingh@gmail.com>
2016-01-07 21:21:00 -0500

    adding jquery

--- src/entry.js
+++ src/entry.js
@@ -1,15 +1,15 @@
 require("./style.css");
 
 var d3 = require("d3");
+var $ = require("jquery");
 window.h = require("h-audio");
 
 import Noise from "./noise.js";
+import EqBars from "./bars.js";
 import Mic from "./microphone.js";
 import oscDataProvider from "./oscillator";
-import hDataProvider from "./h-audio-data-provider";
-import EqBars from "./bars.js";
-
 import SoundCloudSource from "./soundcloud.js";
+import hDataProvider from "./h-audio-data-provider";
 
 var BAR_COUNT = 300;
 
@@ -17,9 +17,6 @@ var mic = new Mic();
 var noise = new Noise();
 var sc = new SoundCloudSource();
 var eqBars = new EqBars(".eq", BAR_COUNT);
-
-let el = (id) => document.getElementById(id);
-let clickListen = (id, func) => el(id).addEventListener("click", func);
 
 function micToggle() {
     mic.toggle();
@@ -45,46 +42,30 @@ function oscToggle() {
 
 function scToggle() {
     sc.toggle();
-
-    // yup
-    let audioEl = el("sc-track");
-    audioEl.getAttribute("hidden") === "" ?
-        audioEl.removeAttribute("hidden") : audioEl.setAttribute("hidden", "");
-
+    $("#sc-track").toggle();
     eqBars.setDataProvider(() => sc.dataProvider().frequency());
 }
 
-var tilted = false;
-var tiltToggle = function tiltToggle() {
-    var eqContainer = document.getElementById("eq-container");
-    if (!tilted) {
-        eqContainer.className = eqContainer.className.replace("tilt", "");
-        eqContainer.className += " tilt";
-        tilted = true;
-    } else {
-        tilted = false;
-        eqContainer.className = eqContainer.className.replace("tilt", "");
-    }
+function tiltToggle() {
+    $("#eq-container").toggleClass("tilt");
 };
 
-
-document.addEventListener("DOMContentLoaded", () => {
-
-    clickListen("noise-play", noiseToggle);
-    clickListen("listen", micToggle);
-    clickListen("tilt", tiltToggle);
-    clickListen("h-audio", hAudioToggle);
-    clickListen("time", hAudioTimeDomainToggle);
-    clickListen("osc", oscToggle);
-    clickListen("soundcloud", scToggle);
+$(function() {
+    $("#noise-play").click(noiseToggle);
+    $("#listen").click(micToggle);
+    $("#tilt").click(tiltToggle);
+    $("#h-audio").click(hAudioToggle);
+    $("#time").click(hAudioTimeDomainToggle);
+    $("#osc").click(oscToggle);
+    $("#soundcloud").click(scToggle);
 
     eqBars.draw();
 
-    sc.setup(el("audio-soundcloud"));
+    sc.setup($("#audio-soundcloud")[0]);
     let trackUrl = "https://soundcloud.com/rsk7/sounds-from-monday-evening";
-    el("trackUrl").value = trackUrl;
+    $("#trackUrl").val(trackUrl);
     sc.track(trackUrl);
-    el("trackUrl").addEventListener("change", e => sc.track(e.target.value));
+    $("#trackUrl").change(e => sc.track(e.target.value));
 });
 
 
